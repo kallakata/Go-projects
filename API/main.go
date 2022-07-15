31063e97fbdbeee472c18fdea58ad26ad12bf9b9
pavel.varenka <pavel.varenka@clevermaps.io>
2022-07-15 15:00:04 +0200

    Service API v1

---
+++ API/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"API/model"
+	"API/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db, err := model.Database()
+	if err != nil {
+		log.Println(err)
+	}
+	db.DB()
+
+	router := gin.Default()
+
+	router.GET("/services", service.GetServices)
+	router.GET("/services/:id", service.GetService)
+	router.POST("/services", service.PostService)
+	router.PUT("/service/:id", service.UpdateService)
+	router.DELETE("/service/:id", service.DeleteService)
+
+	log.Fatal(router.Run(":10000"))
+}
---
+++ API/model/database.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func Database() (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open("./database.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if err = db.AutoMigrate(&Service{}); err != nil {
+		log.Println(err)
+	}
+
+	return db, err
+}
